1005a33fe59690a6b3d1b7ef03a1e551f941828b
asp <asp@aptech.com>
2024-07-31 13:08:43 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'crud-app';
+  products: { id: number,
+    pName: string, 
+    desc: string, 
+    price: string }[] = [];
+  nextId = 1;
+
+  onProductCreate(product: { pName: string, desc: string, price: string }) {
+    const newProduct = { id: this.nextId++, ...product };
+    this.products.push(newProduct);
+    console.log(this.products);
+  }
 }
