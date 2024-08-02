e1fab74b7b5c4059fbee60af0768641457b2a84c
asp <asp@aptech.com>
2024-08-02 12:13:09 +0500

    firebase update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule], 
+  imports: [RouterOutlet, FormsModule, CommonModule, HttpClientModule], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'crud-app';
+
+  constructor(private http: HttpClient){
+
+  }
   products: { id: number,
     pName: string, 
     desc: string, 
@@ -22,5 +27,9 @@ export class AppComponent {
     const newProduct = { id: this.nextId++, ...product };
     this.products.push(newProduct);
     console.log(this.products);
+    this.http.post('https://crud-73a51-default-rtdb.firebaseio.com/products.json', this.products)
+    .subscribe((res) =>{
+      console.log(res)
+    })
   }
 }
